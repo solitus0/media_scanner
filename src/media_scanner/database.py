96413295c63aa5513d2200948c1143a58e1f4e30
Ernestas Pilsudskis <ernestas@ito.lt>
2024-01-21 10:42:41 +0200

    in p

--- src/media_scanner/database.py
+++ src/media_scanner/database.py
@@ -4,10 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-if not os.path.exists("./var"):
-    os.makedirs("./var")
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file_path)
+parent_dir = os.path.dirname(current_directory)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./var/app.sqlite"
+if not os.path.exists(f"{parent_dir}/var"):
+    os.makedirs(f"{parent_dir}/var")
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{parent_dir}/var/app.sqlite"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
